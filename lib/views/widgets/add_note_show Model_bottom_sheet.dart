f6082971d857ebd2a234afa3e060699eb38edd7b
samibarsa <sami5114943@gmail.com>
2023-06-09 11:48:51 +0300

    finsh bottomsheet

--- lib/views/widgets/add_note_show Model_bottom_sheet.dart
+++ lib/views/widgets/add_note_show Model_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note/views/widgets/coustm_text_filed.dart';
+import 'package:note/views/widgets/coustom_Button.dart';
 
 class AddNoteShowbottomsheet extends StatelessWidget {
   const AddNoteShowbottomsheet({super.key});
@@ -8,21 +9,30 @@ class AddNoteShowbottomsheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CoustmTextfiled(
-            hintText: 'Title',
-            maxlines: 1,
-          ),
-          SizedBox(height: 16),
-          CoustmTextfiled(
-            hintText: 'Content ...',
-            maxlines: 5,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CoustmTextfiled(
+              hintText: 'Title',
+              maxlines: 1,
+            ),
+            SizedBox(height: 16),
+            CoustmTextfiled(
+              hintText: 'Content ...',
+              maxlines: 5,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            CoustumBouttun(),
+            SizedBox(
+              height: 25,
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/views/widgets/coustom_Button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:note/costant.dart';
+
+class CoustumBouttun extends StatelessWidget {
+  const CoustumBouttun({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          color: Kprimerycolor, borderRadius: BorderRadius.circular(16)),
+      child: const Center(
+        child: Text(
+          "Add",
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
