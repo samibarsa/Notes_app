9b8722780c759a6b7962f9f50a7259636660c6c7
samibarsa <sami5114943@gmail.com>
2023-06-09 11:29:03 +0300

    add content TextFiled

--- lib/views/widgets/add_note_show Model_bottom_sheet.dart
+++ lib/views/widgets/add_note_show Model_bottom_sheet.dart
@@ -13,7 +13,15 @@ class AddNoteShowbottomsheet extends StatelessWidget {
           SizedBox(
             height: 32,
           ),
-          CoustmTextfiled()
+          CoustmTextfiled(
+            hintText: 'Title',
+            maxlines: 1,
+          ),
+          SizedBox(height: 16),
+          CoustmTextfiled(
+            hintText: 'Content ...',
+            maxlines: 5,
+          )
         ],
       ),
     );
--- lib/views/widgets/coustm_text_filed.dart
+++ lib/views/widgets/coustm_text_filed.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:note/costant.dart';
 
 class CoustmTextfiled extends StatelessWidget {
-  const CoustmTextfiled({super.key});
-
+  const CoustmTextfiled(
+      {super.key, required this.hintText, required this.maxlines});
+  final String hintText;
+  final int maxlines;
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: maxlines,
       cursorColor: Kprimerycolor,
       decoration: InputDecoration(
-          hintText: 'Title',
+          hintText: '${hintText}',
           hintStyle: TextStyle(color: Kprimerycolor),
           border: buildborder(),
           enabledBorder: buildborder(),
