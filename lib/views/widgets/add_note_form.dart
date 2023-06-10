48f2e659a21de3c0e85dbb5e6175cc3b4ea1b4d8
samibarsa <sami5114943@gmail.com>
2023-06-10 21:00:01 +0300

    add loadinindecator to coustom bottoum & add AbsorbPointer

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -46,19 +46,24 @@ class _AddNoteFormState extends State<AddNoteForm> {
           const SizedBox(
             height: 50,
           ),
-          CoustumBouttun(
-            onTap: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-                var notemodel = NoteModel(
-                    title: title!,
-                    subtitle: subTitle!,
-                    date: DateTime.now.toString(),
-                    color: Colors.blue.value);
-                BlocProvider.of<AddNoteCubit>(context).addnote(notemodel);
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-              }
+          BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return CoustumBouttun(
+                isLoading: state is AddNoteLoading ? true : false,
+                onTap: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    var notemodel = NoteModel(
+                        title: title!,
+                        subtitle: subTitle!,
+                        date: DateTime.now.toString(),
+                        color: Colors.blue.value);
+                    BlocProvider.of<AddNoteCubit>(context).addnote(notemodel);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                  }
+                },
+              );
             },
           ),
           const SizedBox(
--- lib/views/widgets/coustom_Button.dart
+++ lib/views/widgets/coustom_Button.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:note/costant.dart';
 
 class CoustumBouttun extends StatelessWidget {
-  const CoustumBouttun({super.key, this.onTap});
+  const CoustumBouttun({super.key, this.onTap, this.isLoading = false});
   final Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -13,12 +14,22 @@ class CoustumBouttun extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
             color: kprimerycolor, borderRadius: BorderRadius.circular(16)),
-        child: const Center(
-          child: Text(
-            "Add",
-            style: TextStyle(
-                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
-          ),
+        child: Center(
+          child: isLoading == true
+              ? const SizedBox(
+                  height: 30,
+                  width: 30,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : const Text(
+                  "Add",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
         ),
       ),
     );
