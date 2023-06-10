333adf2ba563d17dee97d275d790162784d5ab70
samibarsa <sami5114943@gmail.com>
2023-06-10 21:09:43 +0300

    fix keyboard problem

--- lib/views/widgets/add_note_show Model_bottom_sheet.dart
+++ lib/views/widgets/add_note_show Model_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:note/cubites/cubit/add_note_cubit.dart';
 import 'package:note/views/widgets/add_note_form.dart';
 
@@ -12,7 +11,10 @@ class AddNoteShowbottomsheet extends StatelessWidget {
     return BlocProvider(
       create: (context) => AddNoteCubit(),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding: EdgeInsets.only(
+            left: 16,
+            right: 16,
+            bottom: MediaQuery.of(context).viewInsets.bottom),
         child: BlocConsumer<AddNoteCubit, AddNoteState>(
           listener: (context, state) {
             if (state is AddNoteSucsess) {
